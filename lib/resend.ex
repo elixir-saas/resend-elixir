c6f201239592a05501b5c3f3f6344a945161fb2f
Justin Tormey <justin@upsidefinancing.com>
2023-01-25 05:09:57 -0600

    Initial commit

---
+++ lib/resend.ex
@@ -0,0 +1,18 @@
+defmodule Resend do
+  @moduledoc """
+  Documentation for `Resend`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Resend.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/resend_test.exs
@@ -0,0 +1,8 @@
+defmodule ResendTest do
+  use ExUnit.Case
+  doctest Resend
+
+  test "greets the world" do
+    assert Resend.hello() == :world
+  end
+end
