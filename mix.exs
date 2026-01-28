0afb4044f632eec913f7fe8836ad612d9325f56d
Justin Tormey <jrtormey@gmail.com>
2026-01-28 16:07:11 -0600

    v1.0.0-rc.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Resend.MixProject do
   use Mix.Project
 
-  @version "1.0.0-rc.1"
+  @version "1.0.0-rc.2"
   @source_url "https://github.com/elixir-saas/resend-elixir"
 
   def project do
